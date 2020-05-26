63b219e7faea570e157fd93700ec20a837f3e3f8
sysou1 <56151473+sysou1@users.noreply.github.com>
2020-05-26 16:25:19 +0200

    added test for ResourceEvent class

---
+++ ganttproject-tester/test/net/sourceforge/ganttproject/resource/TestResourceEvent.java
@@ -0,0 +1,29 @@
+package net.sourceforge.ganttproject.resource;
+
+import junit.framework.TestCase;
+import net.sourceforge.ganttproject.TestSetupHelper;
+import net.sourceforge.ganttproject.task.TaskManager;
+
+public class TestResourceEvent extends TestCase {
+
+    public void testResourceevent(){
+
+
+        TestSetupHelper.TaskManagerBuilder builder = TestSetupHelper.newTaskManagerBuilder();
+        TaskManager taskManager = builder.build();
+        HumanResourceManager humanResourceManager = builder.getResourceManager();
+
+        HumanResource joe = new HumanResource("Joe", 1, humanResourceManager);
+        HumanResource jane = new HumanResource("Jane", 2, humanResourceManager);
+
+        HumanResource[] resources = {joe, jane};
+
+        ResourceEvent resourceEvent = new ResourceEvent(humanResourceManager, resources);
+
+        assertEquals(humanResourceManager, resourceEvent.getManager());
+        assertEquals(resources, resourceEvent.getResources());
+        assertEquals(joe, resourceEvent.getResource());
+
+    }
+
+}
