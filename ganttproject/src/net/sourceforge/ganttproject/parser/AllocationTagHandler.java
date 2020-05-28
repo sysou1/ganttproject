bec5a5cd34e4aee917a6f09794641e50ff76ad06
sysou1 <56151473+sysou1@users.noreply.github.com>
2020-05-28 22:37:09 +0200

    Removed code duplicates

--- ganttproject/src/net/sourceforge/ganttproject/parser/AllocationTagHandler.java
+++ ganttproject/src/net/sourceforge/ganttproject/parser/AllocationTagHandler.java
@@ -130,24 +130,8 @@ public class AllocationTagHandler extends AbstractTagHandler implements  Parsing
   }
 
   private Role findRole(String persistentIDasString) {
-    RolePersistentID persistentID = new RolePersistentID(persistentIDasString);
-    String rolesetName = persistentID.getRoleSetID();
-    int roleID = persistentID.getRoleID();
-    RoleSet roleSet;
-    if (rolesetName == null) {
-      roleSet = myRoleManager.getProjectRoleSet();
-      if (roleSet.findRole(roleID) == null) {
-        if (roleID <= 10 && roleID > 2) {
-          roleSet = myRoleManager.getRoleSet(RoleSet.SOFTWARE_DEVELOPMENT);
-          roleSet.setEnabled(true);
-        } else if (roleID <= 2) {
-          roleSet = myRoleManager.getRoleSet(RoleSet.DEFAULT);
-        }
-      }
-    } else {
-      roleSet = myRoleManager.getRoleSet(rolesetName);
-    }
-    return roleSet.findRole(roleID);
+    FindRoleClass findRoleClass = new FindRoleClass(myRoleManager);
+    return findRoleClass.findRole(persistentIDasString);
   }
 
   @Override
